2ce6af7dded1040b9e1baf48ddb6ca11d8942a07
Artur T <81235055+a-trzewik@users.noreply.github.com>
2021-08-20 08:55:47 +0200

    initial

---
+++ src/test/java/eu/europa/ec/dgc/validation/DgcaValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.europa.ec.dgc.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgcaValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
