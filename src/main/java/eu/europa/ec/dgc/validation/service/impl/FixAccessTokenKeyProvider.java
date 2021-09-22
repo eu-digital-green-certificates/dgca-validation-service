9824b45a0d4d3e460c87e5a3fc25dcfbc61715a9
a.trzewik@t-systems.com <a.trzewik@t-systems.com>
2021-09-22 11:52:51 +0200

    feat #20

--- src/main/java/eu/europa/ec/dgc/validation/service/impl/FixAccessTokenKeyProvider.java
+++ src/main/java/eu/europa/ec/dgc/validation/service/impl/FixAccessTokenKeyProvider.java
@@ -21,7 +21,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class FixAccessTokenKeyProvider implements AccessTokenKeyProvider {
     private final Map<String,PublicKey> publicKeys = new HashMap<>();
-    private final String UNSET_KEYS_VALUE = "overwrite_my_by_env";
+    private static final String UNSET_KEYS_VALUE = "overwrite_my_by_env";
     private final DgcConfigProperties dgcConfigProperties;
 
     /**
@@ -37,17 +37,17 @@ public class FixAccessTokenKeyProvider implements AccessTokenKeyProvider {
             // developing public key
             // "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEIPrtYsW9+Juwp/mt7h8F"
             //  + "J3LgFRIUl2Vlmcl1DUm5gNHl0LnHIL4Jff6mg6yVhehdQiMvkhUtTvmFIUWONSJEnw=="
-            throw new IllegalArgumentException("please set env variable DGC_ACCESSKEYS for access keys " +
-                "'kid1:publicKey1:kid2:publicKey2'");
+            throw new IllegalArgumentException("please set env variable DGC_ACCESSKEYS for access keys "
+                + "'kid1:publicKey1:kid2:publicKey2'");
         } else {
             String[] keysSplit = keys.split(":");
-            if (keysSplit.length%2 != 0) {
-                throw new IllegalArgumentException("wrong format for access keys env variable: " +
-                    "DGC_ACCESSKEYS, expect: 'kid1:publicKey1:kid2:publicKey2'");
+            if (keysSplit.length % 2 != 0) {
+                throw new IllegalArgumentException("wrong format for access keys env variable: "
+                    + "DGC_ACCESSKEYS, expect: 'kid1:publicKey1:kid2:publicKey2'");
             }
-            for (int i = 0;i<keysSplit.length;i+=2) {
+            for (int i = 0;i < keysSplit.length;i += 2) {
                 String kid = keysSplit[i];
-                byte[] keyBytes = Base64.getDecoder().decode(keysSplit[i+1]);
+                byte[] keyBytes = Base64.getDecoder().decode(keysSplit[i + 1]);
                 X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
                 PublicKey publicKey = kf.generatePublic(spec);
                 publicKeys.put(kid,publicKey);
@@ -60,7 +60,7 @@ public class FixAccessTokenKeyProvider implements AccessTokenKeyProvider {
     public PublicKey getPublicKey(String kid) {
         PublicKey publicKey = publicKeys.get(kid);
         if (publicKey == null) {
-            throw new DccException("can not find access key with kid: "+kid);
+            throw new DccException("can not find access key with kid: " + kid);
         }
         return publicKey;
     }
