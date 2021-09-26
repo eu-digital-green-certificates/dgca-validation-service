749332d3190c96338782b925d21ae4128c562b86
Steffen Schulze <steffen.schulze@t-systems.com>
2021-09-26 12:21:29 +0200

    RCurly fix.

--- src/main/java/eu/europa/ec/dgc/validation/service/ValidationService.java
+++ src/main/java/eu/europa/ec/dgc/validation/service/ValidationService.java
@@ -73,9 +73,8 @@ public class ValidationService {
                 default:
                     log.warn("revoke access token: unsupported algorithm");
                     return null;
-                }
             }
-
+            
             Claims claims = (Claims) token.getBody();
 
             if (claims.containsKey("exp")
